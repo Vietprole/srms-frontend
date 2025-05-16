1ee5ba618daae716ac3f3c59beb28e09c928807f
Vietprole <phuvietdn@gmail.com>
2025-05-16 15:34:16 +0700

    Update base-url

--- src/api/base-url.jsx
+++ src/api/base-url.jsx
@@ -1,8 +1,8 @@
 // const API_BASE_URL = 'http://34.212.98.42';
 // const API_BASE_URL = 'http://34.212.98.42:8080';
 // const API_BASE_URL = 'https://localhost:7018';
-const API_BASE_URL = 'http://localhost:5095';
+// const API_BASE_URL = 'http://localhost:5095';
 // const API_BASE_URL = 'http://27.71.227.212:8080';
-// const API_BASE_URL = 'https://chuandaura.dev.dut.udn.vn';
+const API_BASE_URL = 'https://chuandaura.dev.dut.udn.vn';
 
 export default API_BASE_URL;
