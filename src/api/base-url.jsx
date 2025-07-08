255dce3b0bb5a75dc199a68282d766f28db968ab
Vietprole <phuvietdn@gmail.com>
2025-07-08 22:15:46 +0700

    update base-url

--- src/api/base-url.jsx
+++ src/api/base-url.jsx
@@ -4,6 +4,6 @@
 // const API_BASE_URL = 'http://localhost:5095';
 // const API_BASE_URL = 'http://27.71.227.212:8080';
 // const API_BASE_URL = 'https://chuandaura.dev.dut.udn.vn';
-const API_BASE_URL = 'https://chuandaura.dev.dut.navia.io.vn/';
+const API_BASE_URL = 'https://chuandaura.dev.dut.navia.io.vn';
 
 export default API_BASE_URL;
