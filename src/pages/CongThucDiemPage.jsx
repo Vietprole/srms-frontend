cf52d95db65a7d7ae5df2cff55491a9e5cfd3329
Vietprole <phuvietdn@gmail.com>
2025-07-23 10:40:18 +0700

    Add modal + form

--- src/pages/CongThucDiemPage.jsx
+++ src/pages/CongThucDiemPage.jsx
@@ -61,6 +61,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import BatchUpdateGradeCompositionModal from "@/components/BatchUpdateGradeCompositionModal";
 
 export default function BaiKiemTraPage() {
   const navigate = useNavigate();
@@ -479,6 +480,23 @@ export default function BaiKiemTraPage() {
                 </DropdownMenuContent>
               </DropdownMenu> */}
             <div>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button className="mr-2">Chỉnh sửa hàng loạt</Button>
+                </DialogTrigger>
+                <DialogContent className="max-w-fit">
+                  <DialogHeader>
+                    <DialogTitle>
+                      Chỉnh sửa công thức điểm hàng loạt
+                    </DialogTitle>
+                    <DialogDescription>
+                      Chỉnh sửa công thức điểm cho nhiều lớp học phần
+                    </DialogDescription>
+                  </DialogHeader>
+                  <BatchUpdateGradeCompositionModal />
+                </DialogContent>
+              </Dialog>
+
               <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
                 <DialogTrigger asChild>
                   <Button className="mr-2" disabled={!lopHocPhanId}>
