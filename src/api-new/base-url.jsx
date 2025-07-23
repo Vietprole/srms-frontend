a28aaa11fc5fcf20bb7693b6b8461acc2ff1eca4
Vietprole <phuvietdn@gmail.com>
2025-07-23 21:50:33 +0700

    update base-url

--- src/api-new/base-url.jsx
+++ src/api-new/base-url.jsx
@@ -2,12 +2,12 @@
 // const API_BASE_URL = 'http://34.212.98.42:8080';
 // const API_BASE_URL = 'https://localhost:7018';
 // const API_BASE_URL = 'http://localhost:5095';
-const API_BASE_URL = 'http://localhost:5240';
+// const API_BASE_URL = 'http://localhost:5240';
 
 // const API_BASE_URL = 'https://srms.ielts-msvi.online';
 
 // const API_BASE_URL = 'http://localhost:8085';
 // const API_BASE_URL = 'http://171.244.142.248:8085';
-// const API_BASE_URL = 'https://chuandaura.dev.dut.navia.io.vn';
+const API_BASE_URL = 'https://chuandaura.dev.dut.navia.io.vn';
 
 export default API_BASE_URL;
--- src/api/base-url.jsx
+++ src/api/base-url.jsx
@@ -1,9 +1,9 @@
 // const API_BASE_URL = 'http://34.212.98.42';
 // const API_BASE_URL = 'http://34.212.98.42:8080';
-const API_BASE_URL = 'http://localhost:5240';
+// const API_BASE_URL = 'http://localhost:5240';
 // const API_BASE_URL = 'http://localhost:5095';
 // const API_BASE_URL = 'http://27.71.227.212:8080';
 // const API_BASE_URL = 'https://chuandaura.dev.dut.udn.vn';
-// const API_BASE_URL = 'https://chuandaura.dev.dut.navia.io.vn';
+const API_BASE_URL = 'https://chuandaura.dev.dut.navia.io.vn';
 
 export default API_BASE_URL;
